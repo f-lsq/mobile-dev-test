1f19d25aa38ccf92736731901e57c654bf99099a
f-lsq <fionaloo69@gmail.com>
2024-07-11 00:51:10 +0800

    Completed calling of module and error handling

--- golang-test/greetings/greetings.go
+++ golang-test/greetings/greetings.go
@@ -1,16 +1,24 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // Hello returns a greeting for the named person.
-func Hello(name string) string {
-	// function with capital letter can be called by functions not in the same package
+func Hello(name string) (string, error) {
+	// function with capital letter can be called by functions not in the same package -> exported name
 
-	// Return a greeting that embeds the name in a message.
-	message:= fmt.Sprintf("Hi, %v. Welcome!", name)
+	// If no name was given, return an error with a message
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+
+	//If a name was received, return a greeting that embeds the name in a message.
+	message := fmt.Sprintf("Hi, %v. Welcome!", name)
 	// ':=' is a shortcut for declaring and initialising a variable in one line (value on right is used to determine 'message' variable's type)
 	// long way:
 	// var message string
 	// message = fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
+	return message, nil // 'nil' means no error
 }
--- golang-test/hello/hello.go
+++ golang-test/hello/hello.go
@@ -1,12 +1,30 @@
 package main // declares a main package
+// NOTE: Code executed as an application must be in the main package
 
 import (
 	"fmt"
+	"log"
 
+	"example.com/greetings"
 	"rsc.io/quote"
 ) // imports the 'fmt' package for formatting text
 // package includes printing to the console
 
 func main() { // main function executes by default when you run the main package
 	fmt.Println(quote.Go())
+
+	// Set properties of the predefined Logger, including the log entry prefix and
+	// a flag to disable printing the time, source file and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	// Request a greeting message and print it
+	message, err := greetings.Hello("")
+	// If an error was returned, print it to the console and exit the program
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// If no error was returned, print the returned message to the console
+	fmt.Println(message)
 }
